7f7f713e6780d424bdbf03416dcd7eff66748309
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-12-22 02:52:18 +0300

    feat: hotkey and tooltip for Menu button

--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import clsx from 'clsx';
-import Link from 'next/link.js';
-import { useState } from 'react';
-import { GlobalHotKeys } from 'react-hotkeys';
-import { motion, AnimatePresence } from 'framer-motion';
-import useScroll from '@/features/hooks/useScroll.jsx';
-import MgriLogo from '@/shared/icons/MgriLogo.jsx';
-import Search from '../features/Search.jsx';
-import MenuButton from './menu/MenuButton.jsx';
-import Menu from './menu/Menu.jsx';
-import useThemeSwitch from '@/features/hooks/useThemeSwitch.jsx';
+import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+import Link from "next/link.js";
+import { motion, AnimatePresence } from "framer-motion";
+import Mousetrap from "mousetrap";
+import useScroll from "@/features/hooks/useScroll.jsx";
+import MgriLogo from "@/shared/icons/MgriLogo.jsx";
+import Search from "../features/Search.jsx";
+import MenuButton from "./menu/MenuButton.jsx";
+import Menu from "./menu/Menu.jsx";
+import useThemeSwitch from "@/features/hooks/useThemeSwitch.jsx";
 
 function Header() {
   useThemeSwitch();
@@ -19,75 +19,76 @@ function Header() {
   const [openMenu, setOpenMenu] = useState(false);
 
   const handleOpenMenu = () => {
-    setOpenMenu(!openMenu);
+    setOpenMenu((prevOpenMenu) => !prevOpenMenu);
   };
 
   const handleCloseMenu = () => {
     setOpenMenu(false);
   };
 
-  const keyMap = {
-    SEARCH_SHORTCUT: 'shift+s',
-  };
+  useEffect(() => {
+    Mousetrap.bind("shift+s", handleCloseMenu);
+    Mousetrap.bind("shift+d", handleOpenMenu);
+
+    return () => {
+      Mousetrap.unbind("shift+s");
+      Mousetrap.unbind("shift+d");
+    };
+  }, []);
 
   return (
     <AnimatePresence>
-      <GlobalHotKeys
-        keyMap={keyMap}
-        handlers={{ SEARCH_SHORTCUT: handleOpenMenu }}
+      <motion.header
+        initial={{ y: 0 }}
+        animate={{
+          y:
+            scrollDirection === "up" || openMenu || window.scrollY < 64
+              ? 0
+              : -100,
+        }}
+        exit={{ y: -100 }}
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          damping: 30,
+        }}
+        className={clsx(
+          "flex flex-row justify-between backdrop-blur-xl z-50 sticky top-0 h-16 px-5 transition-colors border-b-1 bg-transparent",
+          {
+            "border-transparent": window.scrollY <= 0 && !openMenu,
+            "border-black/[0.15] dark:border-white/[0.15]":
+              window.scrollY > 0 && !openMenu,
+            "border-[#dcdcdc] dark:border-[#282828]": openMenu,
+          }
+        )}
       >
-        <motion.header
-          initial={{ y: 0 }}
-          animate={{
-            y:
-              scrollDirection === 'up' || openMenu || window.scrollY < 64
-                ? 0
-                : -100,
-          }}
-          exit={{ y: -100 }}
-          transition={{
-            type: 'spring',
-            stiffness: 200,
-            damping: 30,
-          }}
+        <div
           className={clsx(
-            'flex flex-row justify-between backdrop-blur-xl z-50 sticky top-0 h-16 px-5 transition-colors border-b-1 bg-transparent',
+            "absolute top-0 left-0 w-full h-full bg-gradient-to-b from-white dark:from-black to-transparent z-20",
             {
-              'border-transparent': window.scrollY <= 0 && !openMenu,
-              'border-black/[0.15] dark:border-white/[0.15]':
-                window.scrollY > 0 && !openMenu,
-              'border-[#dcdcdc] dark:border-[#282828]': openMenu,
-            },
+              "bg-white/40 dark:bg-black/40": !openMenu,
+              "bg-white dark:bg-black": openMenu,
+            }
           )}
+        />
+        <Link
+          href="/"
+          className="h-full flex items-center gap-[10px] md:gap-5 z-20"
+          onClick={handleCloseMenu}
         >
-          <div
-            className={clsx(
-              'absolute top-0 left-0 w-full h-full bg-gradient-to-b from-white dark:from-black to-transparent z-20',
-              {
-                'bg-white/40 dark:bg-black/40': !openMenu,
-                'bg-white dark:bg-black': openMenu,
-              },
-            )}
-          />
-          <Link
-            href="/"
-            className="h-full flex items-center gap-[10px] md:gap-5 z-20"
-            onClick={handleCloseMenu}
-          >
-            <MgriLogo size={38} />
-            <h1 className="text-sm max-w-[10rem] font-bold md:text-xl md:max-w-fit">
-              Минералогический музей
-            </h1>
-          </Link>
+          <MgriLogo size={38} />
+          <h1 className="text-sm max-w-[10rem] font-bold md:text-xl md:max-w-fit">
+            Минералогический музей
+          </h1>
+        </Link>
 
-          <div className="h-full flex flex-row gap-4 items-center z-20">
-            <Search closeMenu={handleCloseMenu} />
-            <MenuButton isMenuOpen={openMenu} press={handleOpenMenu} />
-          </div>
+        <div className="h-full flex flex-row gap-4 items-center z-20">
+          <Search closeMenu={handleCloseMenu} />
+          <MenuButton isMenuOpen={openMenu} press={handleOpenMenu} />
+        </div>
 
-          <Menu closeMenu={handleCloseMenu} isMenuOpen={openMenu} />
-        </motion.header>
-      </GlobalHotKeys>
+        <Menu closeMenu={handleCloseMenu} isMenuOpen={openMenu} />
+      </motion.header>
     </AnimatePresence>
   );
 }
--- src/widgets/menu/MenuButton.jsx
+++ src/widgets/menu/MenuButton.jsx
@@ -1,21 +1,35 @@
-import { Button } from '@nextui-org/button';
-import clsx from 'clsx';
-import { PiListBold } from 'react-icons/pi';
+import { Button } from "@nextui-org/button";
+import clsx from "clsx";
+import { PiListBold } from "react-icons/pi";
+import { Tooltip } from "@nextui-org/tooltip";
+import { Kbd } from "@nextui-org/kbd";
+import Text from "@/shared/primitives/Text";
 
 function MenuButton({ isMenuOpen, press }) {
   return (
-    <Button
-      type="button"
-      aria-labelledby='Меню'
-      isIconOnly
-      radius="lg"
-      onPress={press}
-      className={clsx('bg-white/60 dark:bg-white/10 shadow-small', {
-        'bg-black text-white dark:bg-white dark:text-black': isMenuOpen,
-      })}
+    <Tooltip
+      classNames={{content: ['p-1 shadow-large']}}
+      offset={10}
+      content={
+        <div className="w-full flex flex-row gap-2 justify-center items-center pl-2">
+          <Text tag="h5" content="Меню" />
+          <Kbd keys={["shift"]}>D</Kbd>
+        </div>
+      }
     >
-      <PiListBold size={20} />
-    </Button>
+      <Button
+        type="button"
+        aria-labelledby="Меню"
+        isIconOnly
+        radius="lg"
+        onPress={press}
+        className={clsx("bg-white/60 dark:bg-white/10 shadow-small", {
+          "bg-black text-white dark:bg-white dark:text-black": isMenuOpen,
+        })}
+      >
+        <PiListBold size={20} />
+      </Button>
+    </Tooltip>
   );
 }
 
